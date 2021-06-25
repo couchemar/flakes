6e886b24e8e638735649067e2f2bae6511c7c4ba
Andrey Pavlov <couchemar@yandex.ru>
2021-06-25 15:00:07 +0300

    Remove default package

--- flake.nix
+++ flake.nix
@@ -1,31 +1,32 @@
 {
   description = "Some useful flakes";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }:
+    with import nixpkgs { system = "x86_64-linux"; }; {
 
-    defaultPackage.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux"; };
-      stdenv.mkDerivation rec {
-        pname = "comby";
-        version = "1.7.0";
-        src = fetchurl {
-          url =
-            "https://github.com/comby-tools/comby/releases/download/${version}/comby-${version}-x86_64-linux";
-          hash = "sha256-WYFTAR6dnDszW5cO2cEFINtk/mAPUq4ZEHCoO0K+1Jc=";
+      packages.x86_64-linux = {
+        comby = stdenv.mkDerivation rec {
+          pname = "comby";
+          version = "1.7.0";
+          src = fetchurl {
+            url =
+              "https://github.com/comby-tools/comby/releases/download/${version}/comby-${version}-x86_64-linux";
+            hash = "sha256-WYFTAR6dnDszW5cO2cEFINtk/mAPUq4ZEHCoO0K+1Jc=";
+          };
+          autoPatchelfIgnoreMissingDeps = true;
+          nativeBuildInputs = [ autoPatchelfHook patchelf ];
+          buildInputs = [ libev zlib sqlite gmp ];
+          dontUnpack = true;
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src $out/bin/comby
+            chmod +x $out/bin/comby
+          '';
+
+          postFixup = ''
+            patchelf --replace-needed libpcre.so.3 ${pcre.out}/lib/libpcre.so $out/bin/comby
+          '';
         };
-        autoPatchelfIgnoreMissingDeps = true;
-        nativeBuildInputs = [ autoPatchelfHook patchelf ];
-        buildInputs = [ libev zlib sqlite gmp ];
-        dontUnpack = true;
-        installPhase = ''
-          mkdir -p $out/bin
-          cp $src $out/bin/comby
-          chmod +x $out/bin/comby
-        '';
-
-        postFixup = ''
-          patchelf --replace-needed libpcre.so.3 ${pcre.out}/lib/libpcre.so $out/bin/comby
-        '';
       };
-  };
+    };
 }
